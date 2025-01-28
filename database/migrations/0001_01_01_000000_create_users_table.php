79ca64c3dca66c6cb41de6555b4e8549a25f0b30
Gleb Starkov <glebstarkov@gmail.com>
2025-01-28 22:22:55 +0800

    Модели и миграции для пользователей и товаров

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->decimal('balance')->default(0);
+            $table->integer('number_of_points')->default(0)->comment('количество баллов');
             $table->rememberToken();
             $table->timestamps();
         });
