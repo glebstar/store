3dd7fbd6e2ca46f55ede6b67dd679e4c8e50c844
Gleb Starkov <glebstarkov@gmail.com>
2025-01-29 21:39:36 +0800

    Добавил проверки при заказе товара на имеющееся количество на складе

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,9 +7,13 @@ use App\Models\Order;
 use App\Models\Product;
 use App\Http\Requests\CreateOrderRequest;
 use App\Http\Requests\ApproveOrderRequest;
+use Illuminate\Validation\ValidationException;
 
 class OrderController extends Controller
 {
+    /**
+     * @throws ValidationException
+     */
     public function createOrder(CreateOrderRequest $request): \Illuminate\Http\JsonResponse
     {
         $order = Order::query()->create([
@@ -18,6 +22,13 @@ class OrderController extends Controller
         ]);
 
         foreach ($request->products as $product) {
+            $product = Product::query()->find($product['id']);
+            if ($product->quantity < $product['quantity']) {
+                throw ValidationException::withMessages([
+                    'products' => ['На складе недостаточно товара.'],
+                ]);
+            }
+
             ItemOrder::query()->create([
                 'order_id' => $order->id,
                 'product_id' => $product['id'],
