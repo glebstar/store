61304800f13ac9d5c83459f73c08411c86f66309
Gleb Starkov <glebstarkov@gmail.com>
2025-01-29 16:13:22 +0800

    Эндпойнт для каталога

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Product;
+use App\Http\Resources\ProductCollection;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/catalog', function () {
+    return new ProductCollection(Product::paginate());
 });
